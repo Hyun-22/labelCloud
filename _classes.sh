f0bf49b4cb157f175e6e446f954f6e09b3575b9a
jonghyun.lee <mickey9624@gmail.com>
2024-01-03 10:05:23 +0900

    add classes sh

---
+++ _classes.sh
@@ -0,0 +1,46 @@
+{
+    "classes": [
+        {
+            "name": "Fault",
+            "id": 0,
+            "color": "#ff0000"
+        },
+        {
+            "name": "Fault(ambi)",
+            "id": 1,
+            "color": "#eb7fff"
+        },
+        {
+            "name": "Vehicle",
+            "id": 2,
+            "color": "#e5d300"
+        },
+        {
+            "name": "Truck",
+            "id": 3,
+            "color": "#9cff00"
+        },
+        {
+            "name": "Pedestrian",
+            "id": 4,
+            "color": "#72e5e5"
+        },
+        {
+            "name": "Cyclist",
+            "id": 5,
+            "color": "#2700ff"
+        },
+        {
+            "name": "Motorcyclist",
+            "id": 6,
+            "color": "#e500d3"
+        }
+    ],
+    "default": 0,
+    "type": "object_detection",
+    "format": "kitti_untransformed",
+    "created_with": {
+        "name": "labelCloud",
+        "version": "1.1.0"
+    }
+}
